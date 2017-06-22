4741c3de9160cac1b985eb015d78b1b9534fc074
Raghav Kumar <raghavku@usc.edu>
2017-06-22 13:55:02 +0200

    Commiting Shiny application, the platform I'm going to use to deploy the code for Graph2Data

--- rsconnect/shinyapps.io/raghavku/Graph2Data.R
+++ rsconnect/shinyapps.io/raghavku/Graph2Data.R
@@ -3,11 +3,11 @@ library(pipeR)
 library(magrittr)
 library(digitize)
 
-im.convert("Train.pdf", output = "bm.png", extra.opts="-density 150")
+#im.convert("Train.pdf", output = "bm.png", extra.opts="-density 150")
 
 Train = ReadAndCal('Train.png') #Load image of the graph and calibrate x and y axis. Click on x1,x2,y1,y2 respectively to load the axes.
 growth = DigitData(col='red') #Click on points within the graph that need to converted into a dataset
-data = Calibrate(growth, Train, 1.3,5,1.2,8.7) #Select the variable containing the point, axes, x1,x2,y1,y2
+data = Calibrate(growth, Train, 1.5,5,2,8) #Select the variable containing the point, axes, x1,x2,y1,y2
 plot(data, type='l') #Plot the data to check if it's representative of the image
 
 data$x = as.integer(data$x) #Convert points on x axis into integers
--- rsconnect/shinyapps.io/raghavku/app.R
+++ rsconnect/shinyapps.io/raghavku/app.R
@@ -1,4 +1,3 @@
-#
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
@@ -6,10 +5,10 @@
 #
 #    http://shiny.rstudio.com/
 #
-
-library(rsconnect)
+library(base)
 library(shiny)
-
+library(digitize)
+source("Graph2Data.R")
 # To upload a file on the app
 
 server <- shinyServer(function(input, output) {
@@ -21,9 +20,17 @@ server <- shinyServer(function(input, output) {
     files
   })
   
+  output$myImage <- renderImage({
+    list(src = "graph.gif", 
+         contentType = 'image/gif',
+         width = 400,
+         height = 200
+    )
+  }, deleteFile=FALSE)
   
   output$images <- renderUI({
     if(is.null(input$files)) return(NULL)
+    
     image_output_list <- 
       lapply(1:nrow(files()),
              function(i)
@@ -31,8 +38,12 @@ server <- shinyServer(function(input, output) {
                imagename = paste0("image", i)
                imageOutput(imagename)
              })
-    
-    do.call(tagList, image_output_list)
+  do.call(tagList, image_output_list)
+  })
+  
+  mode <- reactive({
+    mode <- input$mode
+    ReadAndCal(files)
   })
   
   observe({
@@ -52,17 +63,26 @@ server <- shinyServer(function(input, output) {
       })
     }
   })
-  
 })
 
 ui <- shinyUI(fluidPage(
-  titlePanel("Graph2Data"),
+  titlePanel('Graph2Data'),
   sidebarLayout(
     sidebarPanel(
       fileInput(inputId = 'files', 
                 label = 'Select an Image',
                 multiple = TRUE,
-                accept=c('image/png', 'image/jpeg'))
+                accept=c('image/png', 'image/jpeg')),
+      
+      
+      #insert the button to download the file on main panel:
+      
+      selectInput("mode", "Choose Mode:", 
+                  choices = c("Manual", "Automated")),
+      radioButtons("filetype", "File type:",
+                   choices = c("csv")),
+      downloadButton('downloadData', 'Download Dataset')
+      
     ),
     mainPanel(
       tableOutput('files'),
@@ -70,5 +90,47 @@ ui <- shinyUI(fluidPage(
     )
   )
 ))
+
+#To Download a file 
+
+function(input, output) {
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "iris" = iris,
+           "cars" = mtcars)
+  })
+  
+  output$table <- renderTable({
+    datasetInput()
+  })
+  
+  # downloadHandler() takes two arguments, both functions.
+  # The content function is passed a filename as an argument, and
+  # it should write out data to that filename.
+  
+  output$downloadData <- downloadHandler(
+    
+    # This function returns a string which tells the client
+    # browser what name to use when saving the file.
+    
+    filename = function() {
+      paste(input$mode, input$filetype, sep = ".")
+    },
+    
+    # This function should write data to a file given to it by
+    # the argument 'file'.
+    content = function(file) {
+      sep <- switch(input$filetype, "csv" = ",")
+      
+      # Write to a file specified by the 'file' argument
+      
+      write.table(datasetInput(), file, sep = sep,
+                  row.names = FALSE)
+    }
+  )
+}
+
+
+#Run the application
+
 shinyApp(ui=ui,server=server)
-rsconnect::deployApp()
